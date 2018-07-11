2c640b1394dbcaf5ba2d1e622a31292c8cb85542
hosamsam <hosamelnabawy1996@gmail.com>
2018-07-11 19:34:29 +0200

    implement the buzzer on the arduino board

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // @ts-check
 /* jslint es6 */
-const { Board, Led } = require('johnny-five'),
+const { Board, Led, Piezo } = require('johnny-five'),
       chalk = require('chalk'),
       board = new Board(),
       { log } = console,
@@ -8,6 +8,29 @@ const { Board, Led } = require('johnny-five'),
 
 board.on('ready', () => {
     clog('The Board is Ready!..', 'red');
-    const led1 = new Led(2);
-    led1.blink(1000);
+    const led1 = new Led(5);
+    let buzzer = new Piezo(13);
+    let bright = 0;
+    let ledBrightness = setInterval(()=>{
+        led1.brightness(bright+=10);
+        if (bright>=255) {
+            clearInterval(ledBrightness);
+            led1.on();
+            buzzer.play({
+                tempo: 150, // Beats per minute, default 150
+                song: [ // An array of notes that comprise the tune
+                  [ "c4", 1 ], // Each element is an array in which 
+                               // [0] is the note to play and 
+                               //[1] is the duration in "beats" (tempo, above)
+                  [ "e4", 2 ],
+                  [ "g4", 3 ],
+                  [ null, 4 ] // null indicates "no tone" for the beats indicated
+                ]
+              },()=>{
+                console.log('buzzer sound');
+            });
+        }
+    }, 200);
+
+
 });
